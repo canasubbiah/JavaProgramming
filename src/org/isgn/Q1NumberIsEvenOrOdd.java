22affde9a55d95cb9b19c6bf38b9db9fca54ef64
canasubbiah <kanna608@gmail.com>
2020-07-26 14:22:50 -0400

    Revert Back

--- src/org/isgn/Q1NumberIsEvenOrOdd.java
+++ src/org/isgn/Q1NumberIsEvenOrOdd.java
@@ -2,18 +2,15 @@ package org.isgn;
 
 public class Q1NumberIsEvenOrOdd {
 
-public static void main(String[]args){
-int a = 12;
+	public static void main(String[] args) {
+		int a = 12;
 
-if(a%2==0){
-System.out.println("given number is even");
-}
-else {
-System.out.println("given number is odd");
-}
+		if (a % 2 == 0) {
+			System.out.println("given number is even");
+		} else {
+			System.out.println("given number is odd");
+		}
 
-System.out.println("Kannan GIT testing");
-
-}
+	}
 
 }
